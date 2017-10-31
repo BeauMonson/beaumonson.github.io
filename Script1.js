69547f3188a0b70bb48bfde68ef2cd5bc4b37a4e
BeauMonson <31329882+BeauMonson@users.noreply.github.com>
2017-10-31 13:35:07 -0500

    attempt to keep entered values

--- Script1.js
+++ Script1.js
@@ -17,26 +17,46 @@ function validateEntries() {
 	if (a == "" || a == null)
 	{
 		alert("Please enter your first name.");
+		document.contactForm.lastName.value = b;
+		document.contactForm.email.value = c;
+		document.contactForm.type.value = d;
+		document.contactForm.comment.value = e;
 		return false;
 	}
 	else if (b == "" || b == null)
 	{
 		alert("Please enter your last name.");
+		document.contactForm.firstName.value = a;
+		document.contactForm.email.value = c;
+		document.contactForm.type.value = d;
+		document.contactForm.comment.value = e;
 		return false;
 	}
 	else if (c == "" || c == null)
 	{
 		alert("Please enter your email.");
+		document.contactForm.firstName.value = a;
+		document.contactForm.lastName.value = b;
+		document.contactForm.type.value = d;
+		document.contactForm.comment.value = e;
 		return false;
 	}
 	else if (d == "" || d == null)
 	{
 		alert("Please enter a comment type.");
+		document.contactForm.firstName.value = a;
+		document.contactForm.lastName.value = b;
+		document.contactForm.email.value = c;
+		document.contactForm.comment.value = e;
 		return false;
 	}
 	else if (e == "" || e == null)
 	{
 		alert("Please enter a comment so that we may best serve you.");
+		document.contactForm.firstName.value = a;
+		document.contactForm.lastName.value = b;
+		document.contactForm.email.value = c;
+		document.contactForm.type.value = d;
 		return false;
 	}
 }//end of validateEntries()
