72a4e64f17136d4da2e1f33658d197c54f5c82de
BeauMonson <31329882+BeauMonson@users.noreply.github.com>
2017-11-06 23:40:45 -0600

    undo

--- Script1.js
+++ Script1.js
@@ -54,16 +54,6 @@ function scrollFunction()
   {
     myMenu.classList.remove("sticky");
   }
-
-  var x = document.getElementById("myMenu");
-    if (x.className === "menu") 
-	{
-        x.className += " responsive";
-    } 
-	else 
-	{
-        x.className = "menu";
-    }
 }
 
 function menuFunction() 
