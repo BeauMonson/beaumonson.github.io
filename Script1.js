f9419682e6b754ccec895cc4fbb68baf627f1511
BeauMonson <31329882+BeauMonson@users.noreply.github.com>
2017-11-06 23:39:25 -0600

    trial

--- Script1.js
+++ Script1.js
@@ -54,6 +54,16 @@ function scrollFunction()
   {
     myMenu.classList.remove("sticky");
   }
+
+  var x = document.getElementById("myMenu");
+    if (x.className === "menu") 
+	{
+        x.className += " responsive";
+    } 
+	else 
+	{
+        x.className = "menu";
+    }
 }
 
 function menuFunction() 
