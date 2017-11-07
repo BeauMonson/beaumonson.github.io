c1eff78eba6476159330afca6d29bddafdcd72de
BeauMonson <31329882+BeauMonson@users.noreply.github.com>
2017-11-06 21:47:01 -0600

    menu edit

--- Script1.js
+++ Script1.js
@@ -40,3 +40,16 @@ function validateEntries() {
 		return false;
 	}
 }//end of validateEntries()
+
+function menuFunction() 
+{
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") 
+	{
+        x.className += " responsive";
+    } 
+	else 
+	{
+        x.className = "topnav";
+    }
+}
