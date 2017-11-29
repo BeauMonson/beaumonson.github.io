cb266c7ebd7e7142a094717785c4c55c0f6df4ff
BeauMonson <31329882+BeauMonson@users.noreply.github.com>
2017-11-29 16:56:52 -0600

    speed & stop

--- Final Project/Script-Final.js
+++ Final Project/Script-Final.js
@@ -10,20 +10,26 @@ function myMove() {
   function frame() {
     if (pos == 450) {
       clearInterval(id);
-	  
+	  clearInterval(id2);
     } else {
       pos++; 
       elem.style.left = pos + 'px'; 
     }
   }
+  
+  var ran2 = Math.floor(Math.random()*10);
+
+  while (ran == ran2){
+  	  ran2 = Math.floor(math.random()*10);
+  }
 
   var elem2 = document.getElementById("animate2");
   var pos2 = 0;
-  var ran2 = Math.floor(Math.random()*10);
   var id2 = setInterval(frame2, ran2);
   function frame2() {
   	  if (pos2 == 450) {
 	  	  clearInterval(id2);
+		  clearInterval(id);
 	  } else {
 	  	  pos2++;
 		  elem2.style.left = pos2 + 'px';
