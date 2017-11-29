66790482a12007415dad78291be72080a416edc3
BeauMonson <31329882+BeauMonson@users.noreply.github.com>
2017-11-29 16:38:30 -0600

    undo

--- Final Project/Script-Final.js
+++ Final Project/Script-Final.js
@@ -8,7 +8,7 @@ function myMove() {
   var ran = Math.floor(Math.random()*11);
   var id = setInterval(frame, ran);
   function frame() {
-    if (pos == 90%) {
+    if (pos == 450) {
       clearInterval(id);
     } else {
       pos++; 
