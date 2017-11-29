9c4554d7d4c3190fc0f2525c2bf7d21149a8d8b4
BeauMonson <31329882+BeauMonson@users.noreply.github.com>
2017-11-29 16:47:04 -0600

    two items

--- Final Project/Script-Final.js
+++ Final Project/Script-Final.js
@@ -10,9 +10,23 @@ function myMove() {
   function frame() {
     if (pos == 450) {
       clearInterval(id);
+	  
     } else {
       pos++; 
       elem.style.left = pos + 'px'; 
     }
+  }
+
+  var elem2 = document.getElementById("animate2");
+  var pos2 = 0;
+  var ran2 = Math.floor(Math.random()*11);
+  var id2 = setInterval(frame2, ran2);
+  function frame2() {
+  	  if (pos2 == 450) {
+	  	  clearInterval(id2);
+	  } else {
+	  	  pos2++;
+		  elem2.style.left = pos2 + 'px';
+	  }
   }
 }
